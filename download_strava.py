d899e262e0fb6fff5fe95e28c988050f1c58e3da
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-03-27 16:03:22 -0400

    Progress

--- download_strava.py
+++ download_strava.py
@@ -16,27 +16,35 @@ from logging.handlers import RotatingFileHandler
 from dataclasses import dataclass
 from pathlib import Path
 from stravalib.client import Client
+import subprocess
 
 LOG_NAME = 'strava'
-#  import pandas as pd
+
+
+@dataclass
+class LoginInfo:
+    username: str
+    password_method: str
 
 
 @dataclass
 class StravaInfo:
     client_id: int
     client_secret: str
+    redirect_uri: str
+    scope: str
 
 
-def get_strava_code(client_id):
+def get_strava_code(strava):
     """Use client id to get code"""
     logger = logging.getLogger(LOG_NAME)
-    logger.debug('[get_strava_code]: client_id = %i', client_id)
+    logger.debug('[get_strava_code]: client_id = %i', strava.client_id)
     browser = Browser()
     client = Client()
     url = client.authorization_url(
-        client_id=client_id,
-        redirect_uri='http://127.0.0.1:8000/authorization',
-        scope='read_all')
+        client_id=strava.client_id,
+        redirect_uri=strava.redirect_uri,
+        scope=strava.scope)
     browser.visit(url)
 
 
@@ -79,19 +87,52 @@ def load_strava_info(strava, config):
 
     strava.client_id = int(config['Strava']['client_id'])
     strava.client_secret = config['Strava']['client_secret']
+    strava.redirect_uri = config['Strava']['redirect_uri']
+    strava.scope = config['Strava']['scope']
 
     if not strava.client_id:
         sys.exit("Emtpy client_id")
     if not strava.client_secret:
         sys.exit("Emtpy client_secret")
+    if not strava.redirect_uri:
+        sys.exit("Emtpy redirect_uri")
+    if not strava.scope:
+        sys.exit("Emtpy scope")
+
+
+def get_login_info(login, config):
+    """Get configuration file login info"""
+
+    logger = logging.getLogger(LOG_NAME)
+    login.username = config['Login']['username']
+    login.password_method = config['Login']['password_method']
+
+    if not login.password_method:
+        return
+
+    #  Attempt to get password
+    cmd = login.password_method.replace('"', '')
+    logger.debug('[get_login_info]: cmd = "%s"', cmd)
+    cmd = cmd.split(' ')
+    logger.debug('[get_login_info]: cmd = "%s"', cmd)
+    result = subprocess.run(
+        login.password_method,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    login.password_method = result.stdout.decode('utf-8')
+    logger.debug('[get_login_info]: password = "%s"', login.password_method)
 
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config_filename = 'config.ini'
     strava_info = StravaInfo
+    login = LoginInfo
 
     init_log()
     init_config(config_filename, config)
     load_strava_info(strava_info, config)
-    get_strava_code(strava_info.client_id)
+    get_login_info(login, config)
+    #  get_strava_code(strava_info)
