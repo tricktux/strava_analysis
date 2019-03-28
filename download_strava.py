3f5f4ca0eb1b4bf440731885c7ebf657724a920d
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-03-27 21:29:32 -0400

    Renaming some things

--- download_strava.py
+++ download_strava.py
@@ -29,7 +29,7 @@ class LoginInfo:
 
 
 @dataclass
-class StravaInfo:
+class ApiInfo:
     code: str
     client_id: int
     client_secret: str
@@ -37,23 +37,23 @@ class StravaInfo:
     scope: str
 
 
-def get_strava_code(strava, login):
+def get_strava_code(api, login):
     """Use client id to get code"""
     logger = logging.getLogger(LOG_NAME)
-    logger.debug('[get_strava_code]: client_id = %i', strava.client_id)
+    logger.debug('[get_strava_code]: client_id = %i', api.client_id)
 
     if not login.username or not login.password_method:
         logger.debug(
             '[get_strava_code]: no login info provided. using manual method')
-        strava.code = input('Please enter strava code:\n')
+        api.code = input('Please enter api code:\n')
         return
 
     browser = Browser()
     client = Client()
     url = client.authorization_url(
-        client_id=strava.client_id,
-        redirect_uri=strava.redirect_uri,
-        scope=strava.scope)
+        client_id=api.client_id,
+        redirect_uri=api.redirect_uri,
+        scope=api.scope)
     browser.visit(url)
 
     browser.fill('email', login.username)
@@ -68,7 +68,7 @@ def get_strava_code(strava, login):
     code = browser.url
     logger.debug('[get_strava_code]: raw_code = "%s"', code)
     browser.quit()
-    strava.code = parse_code_url(code)
+    api.code = parse_code_url(code)
     logger.debug('[get_strava_code]: code = "%s"', code)
 
 
@@ -125,23 +125,23 @@ def init_log():
     logger.setLevel(logging.DEBUG)
 
 
-def load_strava_info(strava, config):
+def load_strava_info(api, config):
     """Load and check info from config.ini"""
 
-    strava.client_id = int(config['Strava']['client_id'])
-    strava.client_secret = config['Strava']['client_secret'].replace(
+    api.client_id = int(config['ApiInfo']['client_id'])
+    api.client_secret = config['ApiInfo']['client_secret'].replace(
         '"', '').replace('\'', '')
-    strava.redirect_uri = config['Strava']['redirect_uri'].replace(
+    api.redirect_uri = config['ApiInfo']['redirect_uri'].replace(
         '"', '').replace('\'', '')
-    strava.scope = config['Strava']['scope'].replace('"', '').replace('\'', '')
+    api.scope = config['ApiInfo']['scope'].replace('"', '').replace('\'', '')
 
-    if not strava.client_id:
+    if not api.client_id:
         sys.exit("Emtpy client_id")
-    if not strava.client_secret:
+    if not api.client_secret:
         sys.exit("Emtpy client_secret")
-    if not strava.redirect_uri:
+    if not api.redirect_uri:
         sys.exit("Emtpy redirect_uri")
-    if not strava.scope:
+    if not api.scope:
         sys.exit("Emtpy scope")
 
 
@@ -174,14 +174,14 @@ def get_login_info(login, config):
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config_filename = 'config.ini'
-    strava_info = StravaInfo
+    api_info = ApiInfo
     login = LoginInfo
 
     init_log()
     init_config(config_filename, config)
-    load_strava_info(strava_info, config)
+    load_strava_info(api_info, config)
     get_login_info(login, config)
-    get_strava_code(strava_info, login)
-    print(strava_info.code)
+    get_strava_code(api_info, login)
+    print(api_info.code)
     #  todo rm: parse sample code
     #  http://127.0.0.1:8000/authorization?state=&code=b8231b080d3d565ca6ae342a26417eb2eff6d056&scope=read,read_all
